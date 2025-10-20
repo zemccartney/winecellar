088c4e231275b321d6d50dc2db5ab6eb2143c95c
Zack McCartney <zemccartney@gmail.com>
2025-10-19 22:16:09 -0400

    Post-deploy polish and cleanup

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx()],
-
+  site: "https://wine.grepco.net",
   vite: {
     // TODO delete? still getting re-optimize failures with this on, anyway
     // leaflet and youtube-lite are prone to disappearing, failure to re-optimize; unclear why, try commenting this
